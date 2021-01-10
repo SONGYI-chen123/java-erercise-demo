8258fbe85de2b9f9ec1dd4446ecd246672946f5c
SONGYI-chen123 <song1577660501@163.com>
2021-01-10 23:48:04 +0800

    [syc]: stream flatMap

--- src/main/java/stream/streamLearned.java
+++ src/main/java/stream/streamLearned.java
@@ -11,5 +11,7 @@ public class streamLearned {
         Stream<String[]> stringArray = list.stream().map(s -> s.split(","));
         //以，将字符串进行拆分，返回一个流Stream<String>，返回到新流中
         Stream<Stream<String>> newStream = list.stream().map(s -> Arrays.stream(s.split(",")));
+        //返回一个新流，将每一个元素添加到新流中，与map相比，进行了扁平化操作
+        Stream<String> flatMapStringStream = list.stream().flatMap(s -> Arrays.stream(s.split(",")));
     }
 }
