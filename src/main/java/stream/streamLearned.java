05ec4279f62c2bb47704e2042073316afcf28c61
SONGYI-chen123 <song1577660501@163.com>
2021-01-13 15:48:55 +0800

    [syc]: stream sort

--- src/main/java/stream/streamLearned.java
+++ src/main/java/stream/streamLearned.java
@@ -1,7 +1,11 @@
 package stream;
 
+import parameterization.Apple;
+
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class streamLearned {
@@ -13,5 +17,25 @@ public class streamLearned {
         Stream<Stream<String>> newStream = list.stream().map(s -> Arrays.stream(s.split(",")));
         //返回一个新流，将每一个元素添加到新流中，与map相比，进行了扁平化操作
         Stream<String> flatMapStringStream = list.stream().flatMap(s -> Arrays.stream(s.split(",")));
+    }
+
+    public List<String> naturalSort(List<String> list) {
+        //自然排序，以流中实现的compareTo（）方法进行排序
+//        return list.stream().sorted().collect(Collectors.toList());
+
+        //自然逆序
+        return list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+    }
+
+    public List<Apple> sort(List<Apple> apples) {
+//        return apples.stream()
+//                .sorted((x,y) -> {
+//                    return Integer.compare(x.getWeight(), y.getWeight());
+//                })
+//                .collect(Collectors.toList());
+
+        return apples.stream()
+                .sorted(Comparator.comparing(Apple::getWeight))
+                .collect(Collectors.toList());
     }
 }
